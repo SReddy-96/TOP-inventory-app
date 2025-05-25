e22bf2f2334155335ee6cc5893d2a73cb36ebe83
Steven Reddy <steveng.reddy@gmail.com>
2025-05-25 13:42:17 +1200

    Subject: prod changes

--- db/pool.js
+++ db/pool.js
@@ -8,7 +8,6 @@ module.exports = new Pool({
   database: SECRET.DATABASE,
   password: SECRET.DATABASE_PASSWORD,
   port: 5432,
-  ssl: 'require',
   connectionTimeoutMillis: 5000,
   idleTimeoutMillis: 30000,
 });
